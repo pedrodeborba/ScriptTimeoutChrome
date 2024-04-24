46c76e432e9b67979c6266fe765ec21fa5f0a7a2
pedrodeborba <pedrodeborba1709@gmail.com>
2024-04-24 16:52:21 -0300

    :sparkles: Infinite loop function

--- script.py
+++ script.py
@@ -21,38 +21,42 @@ def close_chrome():
 
 def main():
     # Defina o tempo limite de inatividade em segundos
-    timeout = 120
+    timeout = 10
     timeout_counter = 0  # Inicialize timeout_counter
 
     print("Iniciando monitoramento de inatividade...")
 
     while True:
-        # Obter a posição atual do mouse
-        x, y = pyautogui.position()
+        while True:
+            # Obter a posição atual do mouse
+            x, y = pyautogui.position()
 
-        # Aguardar por 1 segundo
-        time.sleep(1)
+            # Aguardar por 1 segundo
+            time.sleep(1)
 
-        # Verificar se a posição do mouse mudou
-        if pyautogui.position() != (x, y):
-            # O mouse se moveu, resete o contador de tempo
-            timeout_counter = 0
-        else:
-            # O mouse não se moveu, incrementar o contador de tempo
-            timeout_counter += 1
-
-        # Verificar se o contador de tempo atingiu o tempo limite
-        if timeout_counter >= timeout:
-            print("Inatividade detectada por mais de 2 minutos.")
-            # Verificar se o Google Chrome está aberto
-            if is_chrome_open():
-                # Fechar o Google Chrome
-                print("Fechando o Google Chrome...")
-                close_chrome()
-                print("Google Chrome fechado após inatividade.")
+            # Verificar se a posição do mouse mudou
+            if pyautogui.position() != (x, y):
+                # O mouse se moveu, resete o contador de tempo
+                timeout_counter = 0
             else:
-                print("Google Chrome não está aberto.")
-            break
+                # O mouse não se moveu, incrementar o contador de tempo
+                timeout_counter += 1
+
+            # Verificar se o contador de tempo atingiu o tempo limite
+            if timeout_counter >= timeout:
+                print("Inatividade detectada por mais de 2 minutos.")
+                # Verificar se o Google Chrome está aberto
+                if is_chrome_open():
+                    # Fechar o Google Chrome
+                    print("Fechando o Google Chrome...")
+                    close_chrome()
+                    print("Google Chrome fechado após inatividade.")
+                else:
+                    print("Google Chrome não está aberto.")
+                break
+
+        # Aguardar por um período antes de começar a monitorar novamente
+        time.sleep(10)  # Aguarde 10 segundos antes de começar a monitorar novamente
 
 if __name__ == "__main__":
     main()
